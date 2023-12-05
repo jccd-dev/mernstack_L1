4e6e065bc50480da7fd4d1b6759e0b1a817463cd
konnichiwaJCCD <jccd0724@gmail.com>
2023-12-05 13:27:01 +0800

    finish creating backend API in node js - create or post method for books - update or put method - get method - delete method - setup cors config - refactor routing using experess router - created model schema for using mongoose for mongodb

---
+++ Backend/config/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDbURL =
+  "mongodb+srv://root23:root@mernstackl1.olxggjv.mongodb.net/books-collection?retryWrites=true&w=majority";
---
+++ Backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { PORT, mongoDbURL } from "./config/config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+//custom cors config
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Welcome");
+});
+
+//book routes
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDbURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`app is running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
